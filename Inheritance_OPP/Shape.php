92ce4c1ccbb1458b45f984eebdbaf519a3e097be
huyshogun <huy15a3k23@cvp.vn>
2023-08-17 07:37:29 +0700

    first commit

---
+++ Inheritance_OPP/Shape.php
@@ -0,0 +1,11 @@
+<?php
+class Shape {
+    private $name;
+    public function __construct($name) {
+        $this->name = $name
+    }
+    public function showName() 
+    {
+        return "This name is" . $this->name;
+    }
+}
